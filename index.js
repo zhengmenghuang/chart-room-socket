2eec485c1f92df1a0b221850facd537c88c136aa
llxhz <1481010383@qq.com>
2020-07-14 14:10:38 +0800

    push to master: 保留每个房间 前200条的聊天记录

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 ﻿const app = require('express')();
 
-// const https = require('https');
-// const fs = require('fs');
-// const httpsOption = {
-// 	key: fs.readFileSync('./https/3492840_www.llxhzm.xyz.key'),
-// 	cert: fs.readFileSync('./https/3492840_www.llxhzm.xyz.pem')
-// };
-// const port = 6001;
-//
-// const server = https.Server(httpsOption, app);
-
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const httpsOption = {
+	key: fs.readFileSync('./https/3492840_www.llxhzm.xyz.key'),
+	cert: fs.readFileSync('./https/3492840_www.llxhzm.xyz.pem')
+};
 const port = 6001;
-const server = http.Server(app);
+
+const server = https.Server(httpsOption, app);
+
+// 本地开发
+// const http = require('http');
+// const port = 6001;
+// const server = http.Server(app);
 
 const io = require('socket.io')(server);
 
@@ -21,6 +22,9 @@ const utils = require('./utils/util')
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+// 全局存储聊天记录
+const chartRecord = new Map();
 
 io.on('connection', function (socket) {
   // 监听上线
@@ -41,17 +45,24 @@ io.on('connection', function (socket) {
 
   // 加入房间
   socket.join(roomId);
+  if (!chartRecord.has(roomId)) {
+    chartRecord.set(roomId, []);
+  }
 
 
   // 转发广播消息
   socket.on('msg', msg => {
     io.to(roomId).emit('msg', msg);
+    chartRecord.get(roomId).push(msg);
+    if (chartRecord.get(roomId).length > 200) {
+      chartRecord.set(roomId, chartRecord.get(roomId).slice(-200))
+    }
   });
 
-  // 监听用户加入
+  // 监听用户加入 先发历史记录 再发上线记录
   socket.on('join', m => {
-    console.log(m)
     info = m;
+    socket.emit('historyRecord', chartRecord.get(roomId));
     io.to(roomId).emit('msg', {
       type: 'join',
       value: {
