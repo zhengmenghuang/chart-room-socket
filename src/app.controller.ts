d9e999c4a8f66a504ffc5cc9b82d148202d1ef72
llxhz <1481010383@qq.com>
2020-07-23 16:07:16 +0800

    push to master: 配置好静态资源服务器的配置 Gzip 404等

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render, Response } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root(@Response() res) {
+    // res.sendFile(join(__dirname, '../views', '/index.hbs'));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as compression from 'compression';
+import * as history from 'connect-history-api-fallback';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 开启Ｇzip
+  app.use(compression());
+
+  // 配置跨域
+  app.enableCors();
+
+  // 配置刷新404
+  app.use(history({ verbose: true, index: '/'}));
+
+  // 配置读取静态资源和view文件夹
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 bootstrap();
